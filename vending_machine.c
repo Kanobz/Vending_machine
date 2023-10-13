8e1729509b275362998e3ebef20cfa001c18665f
Kanobz <fmk247@uowmail.edu.au>
2023-10-13 12:22:20 +0400

    Added more code to the admin mode and coin input

--- vending_machine.c
+++ vending_machine.c
@@ -1,65 +1,58 @@
 #include <stdio.h>
 int main()
 {
-    int menu, purchase, admin, exit, password;
+    int menu, purchase, A, B, C, amount_paid, coin_options, admin, exit, password;
     while (1){
-    printf("Hi! Welcome to the UOWD Vending Machine! \n\n What would you like to do? \n");
+    printf("\nHi! Welcome to the UOWD Vending Machine! \n\nWhat would you like to do? \n");
     printf("1. Purchase Item for the customer \n");
     printf("2. Admin mode for the Vending machine operator \n");
     printf("3. Exit \n\n");
-    printf("Select an option:  ");
+    printf("Select an option: ");
     scanf("%d",&menu);
 
      switch (menu) {
             case 1:
                 while (1) { 
                     // Purchasing item
-                    printf("\nHere are the products available: \n");
-                    printf("1. A for AED 3.25 \n");
-                    printf("2. B for AED 2 \n");
-                    printf("3. C for 4.5 \n\n");
+                    A = 3.25, B =2.0, C = 4.5, amount_paid = 0, coin_options = [1, 0.25, 0.5];
+                    printf("\nHere are the products availableand their quantities: \n");
+                    printf("1. A for AED 3.25 (8)\n");
+                    printf("2. B for AED 2.0 (3)\n");
+                    printf("3. C for 4.5 (5)\n\n");
                     
                     printf("Enter your product of choice: ");
                     scanf("%d", &purchase);
 
-                    printf("\nYou have selected option %d\n ", purchase);
-                    printf("If correct, enter 'y' to continue. If incorrect, enter 0 to cancel purchase - \n");
+                    printf("\nYou have selected option %d which costs AED %.2f\n", purchase);
+                    scanf("%d%f", &purchase);
+                    printf("If correct, enter 'y' to continue. If incorrect, enter 0 to cancel purchase - ");
                     scanf("%c%d", &purchase);
+                    
+                    if(purchase!=0){
+                        while (amount_paid < A || B || C){
+                           printf("\nPay for the item by inserting one of these 3 coins: AED 1, AED 0.5, AED 0.25");
+                           scanf("%f",&coin_options);
+                     
+                           if (coin_options == 1 || coin_options == 0.25 || coin_options == 0.5 ||){
+                              amount_paid += coin_options;
+                              printf("Total amount paid: AED %.2f\n", amount_paid);}
+                             else 
+                             printf("Invalide coin. Please insert a valid coin.");
+                           }
+                          printf("Thank you! Here is your item.\n"); 
 
-                    if(purchase==0)
+                          return 0;
+                                   }
+                    else
                     printf("Purchase Cancelled.");
-                    return purchase;
-
-
-/*
-                    switch (subChoice) {
-                        case 1:
-                            printf("You selected Sub-Option 1.\n");
-                            break;
-                        case 2:
-                            printf("You selected Sub-Option 2.\n");
-                            break;
-                        case 3:
-                            printf("Returning to Main Menu.\n");
-                            break;
-                        default:
-                            printf(" \n");
-                    }
-
-                    if (subChoice == 3) {
-                        break; // Exit the sub-menu loop and return to the main menu
-                    }
-                }
-                break;
-             switch () 
-             */  
+                    return menu; break;
             case 2: // Admin mode
                 while (1) { 
                     printf("Welcome to the Admin Mode! \n");
                     printf("Please enter the password: \n");
-                    scanf("%d",admin);
+                    scanf("%d",password);
 
-                if (password==2020)
+                if (password==2020){
                     printf("What would you like to do: \n");
                     printf("1. Replenish items \n");
                     printf("2. Change item prices \n");
@@ -69,11 +62,26 @@ int main()
 
                     printf("Select option: ");
                     scanf("%d", &admin);
-                else if
-                    printf("Password incorrect. Please try again.");
-                   scanf("%d",&admin);
-                else
-                   printf("Password incorrect. Exiting ADMIN MODE."); break;
+                    
+                    switch (admin){
+                        case 1: //Replenish items
+                            printf("You selected Sub-Option 1.\n");
+                            break;
+                        case 2: //Change item prices
+                            printf("You selected Sub-Option 2.\n");
+                            break;
+                        case 3: //Display total sales
+                            printf("Returning to Main Menu.\n");
+                            break;
+                        case 4: //Display item availability    
+                        default:
+                            printf("Exit Admin mode\n");     
+                    }
+                  else if {
+                    printf("Password incorrect. Please try again."); break;
+                    scanf("%d",&password);}
+                else{
+                    printf("Password incorrect. Exiting ADMIN MODE.");} break;
 
             default:
                 printf("Thank you! \n");
@@ -82,7 +90,7 @@ int main()
 
     return 0;
 
-}
-}
-}
+   }
+  }
+ }
 }
